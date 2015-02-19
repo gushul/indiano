08ef77657cfb5101eefe31977fd2ae6f1b68f5d3
ueiek <ueieko@gmail.com>
2015-02-19 10:34:45 +0300

    FiX: carrierwave s3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   include CarrierWave::MiniMagick
 
-  storage :file
+  storage :fog
   # Choose what kind of storage to use for this uploader:
 
   # Override the directory where uploaded files will be stored.
