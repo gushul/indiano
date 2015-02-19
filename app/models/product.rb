70309cc8d41bc823df4653a95787d8345ec249b2
ueiek <ueieko@gmail.com>
2015-02-19 13:03:54 +0300

    Fix: delete carrierwave config from initialize

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :pictures
   belongs_to :category
-  accepts_nested_attributes_for :pictures
+  accepts_nested_attributes_for :pictures, allow_destroy: true
 
   scope :category_id, -> (category_id) { where category_id: category_id }
 end
