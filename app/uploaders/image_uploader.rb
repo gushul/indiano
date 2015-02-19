b54c9369b33295bc16f8414b1ad5ba7af800989e
ueiek <ueieko@gmail.com>
2015-02-19 12:14:52 +0300

    Fix: carriearwave add  cache dir

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,13 +5,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   include CarrierWave::MiniMagick
 
-  storage :fog
+  storage :file
   # Choose what kind of storage to use for this uploader:
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  def cache_dir
+    '/tmp/application-cache'
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
