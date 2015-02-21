7c8d382a855d8f8829357126a151ef4d8bb255c4
ueiek <ueieko@gmail.com>
2015-02-21 08:45:48 +0300

    Featuer: Add validation to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,8 @@ class Product < ActiveRecord::Base
   belongs_to :category
   accepts_nested_attributes_for :pictures, allow_destroy: true
 
+  validates :title, presence: true
+  validates_associated :category
+
   scope :category_id, -> (category_id) { where category_id: category_id }
 end
