f8fc36e2dd2fcf82cb508d4883b5aa8371e8a84a
Oleg Gushul <ueieko@gmail.com>
2015-04-14 13:45:01 +0300

    Fix: static page routes from root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Indiano::Application.routes.draw do
   devise_for :admins
   resources :categories
-
-  root "static_pages#home"
-  get "static_pages/about",   as: :about
-  get "static_pages/contact", as: :contact
-  get "static_pages/buy",     as: :how_to_buy
   resources :products
   resources :pictures
   resource :shopping_carts
