a51135ef553b582178f1f4aff4ce774b6974ac70
Oleg Gushul <ueieko@gmail.com>
2015-04-18 07:06:39 +0300

    Header: variant 2 version 0.5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
+//
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -14,8 +15,8 @@
 //= require fotorama
 //= require jquery_ujs
 //= require ckeditor-jquery
-//= require bootstrap.min
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
 $(document).on("ready page:change", function() {
   $('.fotorama').fotorama();
