23b5cf651c575961135fb739f4d898405b065f8a
Oleg Gushul <ueieko@gmail.com>
2015-08-01 13:37:14 +0300

    Remove Admin view and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Indiano::Application.routes.draw do
   mount_roboto
-  devise_for :admins
   resources :categories
   resources :products
   resources :pictures
   resource :shopping_carts
-  devise_scope :admin do
-    get "/admin" => "devise/sessions#new"
-  end
   get 'sitemap' => 'home#sitemap'
   get "/catalog_pdf" => 'home#catalog_pdf'
 
